04a3133ce74f5c23caf5da2380f0ec84433f62c1
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-28 20:57:45 +0900

    Add solution to leetcode 1207

---
+++ 1207-Unique Number of Occurrences.py
@@ -0,0 +1,19 @@
+# Link to question: https://leetcode.com/problems/unique-number-of-occurrences/
+
+def uniqueOccurrences(arr):
+    num_count = {}
+    
+    for num in arr:
+        if num in num_count:
+            num_count[num] += 1
+        else:
+            num_count[num] = 1
+    
+    unique_occ = set()
+    for num, occ in num_count.items():
+        if occ in unique_occ:
+            return False
+        else:
+            unique_occ.add(occ)
+    
+    return True
